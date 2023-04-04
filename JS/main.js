a90b77baba01742085ebdeeb11c1788cc13e89bf
Astrid <35829@ma-web.nl>
2023-04-04 11:57:32 +0200

    changes

--- JS/main.js
+++ JS/main.js
@@ -13,6 +13,18 @@ setInterval(() =>{
 },1000)
 
 let weatherDegrees = document.getElementById("js--weather-degrees");
-console.log(weatherDegrees);
+let weatherMinDegrees = document.getElementById("js--weather-minDegrees");
+let weatherMaxDegrees = document.getElementById("js--weather-maxDegrees");
 
+let currentWeather = fetch("https://api.open-meteo.com/v1/forecast?latitude=52.37&longitude=4.89&hourly=temperature_2m&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum&forecast_days=1&timezone=auto")
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(realData){
+    weatherMinDegrees.innerHTML = realData.daily.temperature_2m_min;
+    weatherMaxDegrees.innerHTML = realData.daily.temperature_2m_max;
+    if(realData.current_weather.time == realData.hourly.time){
+        realData.hourly.temperature_2m;
+    }
+    });
 
